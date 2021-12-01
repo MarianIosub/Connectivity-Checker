d8cbe45eb2b0f31712813ce7f4e6084b30433a1d
Iosub Ovidiu-Marian <63242787+MarianIosub@users.noreply.github.com>
2021-12-01 18:34:44 +0200

    Added main structure

--- Connectivity-Checker/check_connectivity.py
+++ Connectivity-Checker/check_connectivity.py
@@ -1,1 +1,21 @@
+import sys
 
+
+def main():
+    if sys.argv[1] == '-url':
+        print("url")
+    elif sys.argv[1] == '-ftp':
+        print("ftp")
+    elif sys.argv[1] == '-mongodb':
+        print("mongodb")
+    elif sys.argv[1] == '-postgresql':
+        print("postgres")
+    elif sys.argv[1] == '-elastic':
+        print("elastic")
+    else:
+        print("Invalid domain for checking!")
+        return
+
+
+if __name__ == '__main__':
+    main()
