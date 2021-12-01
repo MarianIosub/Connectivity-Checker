ffdfa0c35403d88b6c7809f164c08d967dc385f3
Iosub Ovidiu-Marian <63242787+MarianIosub@users.noreply.github.com>
2021-12-01 19:02:48 +0200

    Added checking HTTP

--- Connectivity-Checker/check_connectivity.py
+++ Connectivity-Checker/check_connectivity.py
@@ -1,9 +1,27 @@
 import sys
+import urllib
+from urllib import request
+from urllib import error
+
+
+def url_check(url):
+    if url[0:3] == "www":
+        url = "https://" + url
+    try:
+        status_code = urllib.request.urlopen(url).getcode()
+    except urllib.error.HTTPError:
+        print("False")
+        return
+    except urllib.error.URLError:
+        print(False)
+        return
+    website_is_up = status_code == 200.
+    print(website_is_up)
 
 
 def main():
     if sys.argv[1] == '-url':
-        print("url")
+        url_check(sys.argv[2])
     elif sys.argv[1] == '-ftp':
         print("ftp")
     elif sys.argv[1] == '-mongodb':
