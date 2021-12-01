17aaf48da2351e3337f96d5b56bfbabc5b3ec871
Iosub Ovidiu-Marian <63242787+MarianIosub@users.noreply.github.com>
2021-12-01 19:41:39 +0200

    Added check for postgresql

--- Connectivity-Checker/check_connectivity.py
+++ Connectivity-Checker/check_connectivity.py
@@ -3,6 +3,9 @@ import urllib
 from urllib import request
 from urllib import error
 
+import psycopg2
+from urllib.parse import urlparse
+
 
 def url_check(url):
     if url[0:3] == "www":
@@ -19,6 +22,31 @@ def url_check(url):
     print(website_is_up)
 
 
+def ftp_check(ftp_link):
+    pass
+
+
+def check_postgres(uri):
+    # postgres: // mdtdoxetgisriv: e980d8c00263e2fb362485c68e4933987f3df27c8daeaabfd3dc2b167984dcd7 @ ec2 - 176 - 34 - 105 - 15.eu - west - 1.compute.amazonaws.com: 5432 / d1qujjivcif155
+    result = urlparse(uri)
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+    try:
+        _ = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        print("True")
+    except Exception as e:
+        print("False: " + str(e))
+
+
 def main():
     if len(sys.argv) != 3:
         print("Invalid number of arguments!")
@@ -26,11 +54,11 @@ def main():
     if sys.argv[1] == '-url':
         url_check(sys.argv[2])
     elif sys.argv[1] == '-ftp':
-        print("ftp")
+        ftp_check(sys.argv[2])
     elif sys.argv[1] == '-mongodb':
         print("mongodb")
     elif sys.argv[1] == '-postgresql':
-        print("postgres")
+        check_postgres(sys.argv[2])
     elif sys.argv[1] == '-elastic':
         print("elastic")
     else:
